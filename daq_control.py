23e6f976e1036c6c2b6b992879f80975606128e2
Dave Williams <cdave@uw.edu>
2016-04-21 09:50:22 -0500

    Cleaned and documented DAQ control

--- daq_control.py
+++ daq_control.py
@@ -25,21 +25,14 @@ class daq_acquire(object):
     def __init__(self):
         """Set up the channels and such"""
         ## Input channel configuration
-        #DEV = r'Dev1'               # device
-        #STIM_IN = DEV+r'/ai0'       # stimulus signal
-        #LENGTH_IN = DEV+r'/ai1'     # length from motor
-        #BEAM_IN = DEV+r'/ai2'       # beam shutter
-        #EXPOSURE_IN = DEV+r'/ai3'   # exposure trigger
-        #FORCE_IN = DEV+r'/ai4'      # force transducer
-        #PSD1 = DEV+r'/ai5'          # PSD side 1
-        #PSD2 = DEV+r'/ai6'          # PSD side 2
-        #ALL_IN = [STIM_IN, LENGTH_IN, BEAM_IN, EXPOSURE_IN, FORCE_IN,
-        #          PSD1, PSD2]
-        #NAMES_IN = [r'stim', r'length', r'beam', r'exposure', r'force',
-        #            r'psd1', r'psd2']
         self.nchannels = 7
-        self.channelnames = ['length', 'force', 'stimulation', 
-                             'beam', 'exposure', 'psd1', 'psd2']
+        self.channelnames = ['length',      # muscle lever output
+                             'force',       # force transducer output
+                             'stimulation', # sync signal from TI's stim
+                             'beam',        # bpm diode upstream of sample
+                             'exposure',    # pilatus trigger signal
+                             'psd1',        # output from top psd connection
+                             'psd2']        # bottom psd connection
         ## Sample rate configuration
         self.samplerate = 10000
         self.nsamples = 10000 # if samplemode = 'continuous', 
@@ -58,15 +51,14 @@ class daq_acquire(object):
         """Set up an analog input task"""
         n = str(self.nchannels - 1)
         self.itask = nidaqmx.AnalogInputTask()
-        #self.itask.create_voltage_channel(r'Dev1/ai2:7',
-        self.itask.create_voltage_channel(r'Dev1/ai0:'+n,
-                                      min_val=-10.0, max_val=10.0,
-                                      terminal=self.TERMINALEND)
+        self.itask.create_voltage_channel(
+                                r'Dev1/ai0:'+n, # create n AIs
+                                min_val=-10.0,  # min at -10V
+                                max_val=10.0,   # max at 10V
+                                terminal=self.TERMINALEND)
         self.itask.configure_timing_sample_clock(rate=self.samplerate,
                                       sample_mode=self.samplemode,
                                       samples_per_channel=self.nsamples)
-        ## print "input task buffer size", itask.get_buffer_size()
-        ## print itask.get_read_current_position()
     
     def start_task(self):
         """Begin the collection and wait for it to be completed"""
@@ -75,7 +67,7 @@ class daq_acquire(object):
     def read_data(self):
         """Read the data off the DAQ's buffer"""
         self.itask.wait_until_done(10.0)
-        raw= self.itask.read()
+        raw = self.itask.read()
         deinterleaved = [[] for i in raw[0]]
         for sample in raw:
             for i, datum in enumerate(sample):
