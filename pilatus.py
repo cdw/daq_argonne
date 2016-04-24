a77dc8ca8403b2fb77d4beeeabcd547896e2dd98
Dave Williams <cdave@uw.edu>
2016-04-24 14:24:48 -0500

    Basedir back to QR, update trig and fn
    Trigger mode and filename additions are now set back to the values we
    want since they get changed for other experiments

--- pilatus.py
+++ pilatus.py
@@ -9,12 +9,15 @@ If something looks wrong, debug by watching the output on the cam_server window
 Created by Dave Williams on 2014-12-04
 """
 
+import os
 import time
 import epics
 from epics import pv
 
 
-BASE_DIR = '/nas_data/2016DanielAnt/'
+#TODO: Pass in basedir
+
+BASE_DIR = '/nas_data/2016DanielQR/'
 
 BASE_EPICS = "18ID-2:PILATUS1:"
 
@@ -28,6 +31,8 @@ def set_up_pilatus(fn, exp_time, base_dir=BASE_DIR):
     aquire_button = pv.PV(BASE_EPICS+"Acquire")
     filepath_entry = pv.PV(BASE_EPICS+"FilePath")
     filename_entry = pv.PV(BASE_EPICS+"FileName")
+    template_entry = pv.PV(BASE_EPICS+"FileTemplate")
+    trigger_entry = pv.PV(BASE_EPICS+"TriggerMode")
     # Set up messages to send
     exp_msg = (exp_time/1000000.0) # convert to sec
     fn_msg = fn
@@ -36,4 +41,6 @@ def set_up_pilatus(fn, exp_time, base_dir=BASE_DIR):
     filepath_entry.put(base_dir)
     filename_entry.put(fn)
     aquire_button.put(1)
+    template_entry.put("%s%s")
+    trigger_entry.put(2)
     return base_dir+fn
